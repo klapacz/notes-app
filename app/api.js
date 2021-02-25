13dba34592a87e60c7e7b3092c803b72b6be6816
Korneliusz Łapacz <lapacz.kornel@gmail.com>
2021-02-25 14:14:54 +0100

    sanitize response

--- app/api.js
+++ app/api.js
@@ -3,7 +3,12 @@ const router = express.Router();
 
 module.exports = db => {
 	router.get('/', async (req, res) => {
-		const val = await db.value();
+		const val = await db
+			.map(user => {
+				delete user.password;
+				return user
+			})
+			.value();
 
 		res.smartSend(val);
 	})
