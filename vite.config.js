7a7571fe1119d684abca4dec39ec48223c30da14
Korneliusz Łapacz <lapacz.kornel@gmail.com>
2021-03-09 16:12:49 +0100

    svg loader

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
 import { defineConfig } from 'vite';
 import dotenv from 'dotenv-flow';
 import vue from '@vitejs/plugin-vue';
+import vueSvgPlugin from 'vite-plugin-vue-svg';
 
 // vite loads .env only to app source
 dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [vue()],
+	plugins: [vue(), vueSvgPlugin()],
 	base: process.env.BASE_URL,
 	server: {
 		proxy: {
